5aadfea484e9f7e4d8f5ef6de07b8b7dbb9e4b3b
Unknown <hernaultcontact@gmail.com>
2018-04-15 16:04:46 +0200

    Revert "Cleaning"
    This reverts commit e5fefc4a9ee59fb07bdccf3ca0836345fb14c724.

--- src/LFP/TimbalBundle/Controller/CourseController.php
+++ src/LFP/TimbalBundle/Controller/CourseController.php
@@ -96,6 +96,8 @@ class CourseController extends Controller
             $dayRanks = [];
         }
 
+        // NOTE: simplifier le render
+
         $content = $this->get('templating')->render('LFPTimbalBundle:Course:menu.html.twig', array(
             'userCourses' => $userCourses,
             'dayRanks' => $dayRanks,
@@ -159,6 +161,8 @@ class CourseController extends Controller
             $dayRanks = [];
         }
 
+        // -----------------------------------------------------------------------
+
         $snappyStyles = true;
         $snappy = $this->get("knp_snappy.pdf");
         $snappy->setOption("encoding", "UTF-8");
@@ -166,6 +170,8 @@ class CourseController extends Controller
         // ------check numbers of column to resize them properly in pdf view----//
 
         $numberOfColumns = $this->getDoctrine()->getManager()->getRepository('LFPTimbalBundle:Course')->countDays($em, $currentUser);
+
+        // ---------------------------------------------------------------------//
 
         $html = $this->renderView("LFPTimbalBundle:Course:menu.html.twig", array(
             "dayRanks" => $dayRanks,
--- src/LFP/UserBundle/Controller/SecurityController.php
+++ src/LFP/UserBundle/Controller/SecurityController.php
@@ -11,6 +11,7 @@ use LFP\UserBundle\Form\UserType;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+
 class SecurityController extends Controller
 {
     /**
@@ -18,11 +19,20 @@ class SecurityController extends Controller
      */
     public function loginAction(Request $request)
     {
+        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        //--------------------------------
+        // NOTE: ne marche pas
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             return $this->redirectToRoute('lfp_timbal_new');
         }
+        //---------------------------------------
 
+
+        // Le service authentication_utils permet de récupérer le nom d'utilisateur
+        // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+        // (mauvais mot de passe par exemple)
         $authenticationUtils = $this->get('security.authentication_utils');
+
 
         return $this->render('LFPUserBundle:Security:login.html.twig', array(
       'last_username' => $authenticationUtils->getLastUsername(),
@@ -31,13 +41,16 @@ class SecurityController extends Controller
     }
 
     /**
+     * Matches /id exactly.
      * @Route("/id", name="lfp_timbal_id")
      */
-    public function idAction(Request $request)
+    public function idAction(Request $request /*,UserPasswordEncoderInterface $encoder*/)
     {
+
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_USER')) {
             return $this->redirectToRoute('lfp_timbal_new');
         }
+
 
         $this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_ANONYMOUSLY');
         $user = new User();
@@ -45,16 +58,23 @@ class SecurityController extends Controller
         $form = $this
             ->get('form.factory')
             ->create(UserType::class, $user)
-        ;
+            ;
+
 
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
+            // $plainPassword = $form->getData()->getPassword();
+            // $encoded = $encoder->encodePassword($user, $plainPassword);
+            //
+            // $user->setPassword($encoded);
+
             $user->setSalt('');
             $user->setRoles(array('ROLE_USER'));
 
             $em = $this
                 ->getDoctrine()
                 ->getManager()
-            ;
+                ;
             $em->persist($user);
             $em->flush();
 
@@ -62,18 +82,21 @@ class SecurityController extends Controller
                 ->getSession()
                 ->getFlashBag()
                 ->add('notice', 'Hello ' . ucfirst($user->getUsername()) . ' your Timbal is here')
-            ;
+                ;
 
 
             return $this->redirectToRoute('lfp_timbal_new');
         }
 
+        // $authenticationUtils = $this->get('security.authentication_utils');
+
         return $this->render('LFPUserBundle:Security:id.html.twig', array(
-                'form' => $form->createView(), ));
+            'form' => $form->createView(), ));
     }
 
     public function passwordEncription(UserPasswordEncoderInterface $encoder)
     {
+        // whatever *your* User object is
         $user = new User();
         $plainPassword = $form->getData()->getPassword();
         $encoded = $encoder->encodePassword($user, $plainPassword);
